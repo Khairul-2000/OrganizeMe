cbfb41b83b9451579cd57abba5794f7125c13b1a
Khairul-2000 <kahirulislam135@gmail.com>
2025-01-17 23:27:04 +0600

    user already exits added

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -22,76 +22,74 @@ const Register = () => {
     if (user.ok) {
       alert("User created successfully");
       navigate("/signin");
+    } else {
+      alert("User already exists");
     }
   }
   return (
     <div>
       <div className="m-auto mt-[25vh] flex max-w-[400px] items-center justify-center rounded-md bg-white py-8">
-        {loading ? (
-          <p className="animate-spin text-7xl"> ⏳</p>
-        ) : (
-          <form
-            onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
-              e.preventDefault();
-              const form = e.currentTarget;
-              const formData = new FormData(form);
-              const data = {
-                username: formData.get("username"),
-                email: formData.get("email"),
-                password: formData.get("password"),
-              };
-              console.log("FormData: ", data);
+        <form
+          onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
+            e.preventDefault();
+            const form = e.currentTarget;
+            const formData = new FormData(form);
+            const data = {
+              username: formData.get("username"),
+              email: formData.get("email"),
+              password: formData.get("password"),
+            };
+            console.log("FormData: ", data);
 
-              register(data);
-              form.reset();
-            }}
-          >
-            <div className="mb-6 flex flex-row items-center justify-between">
-              <div className="flex items-center gap-2">
-                <img src="todo.png" alt="todo" className="w-8" />
-                <h1 className="text-2xl font-extrabold text-green-600">TODO</h1>
-              </div>
-
-              <RxCross1
-                size={25}
-                color="black"
-                onClick={() => navigate("/")}
-                className="cursor-pointer"
-              />
+            register(data);
+            form.reset();
+          }}
+        >
+          <div className="mb-6 flex flex-row items-center justify-between">
+            <div className="flex items-center gap-2">
+              <img src="todo.png" alt="todo" className="w-8" />
+              <h1 className="text-2xl font-extrabold text-green-600">TODO</h1>
             </div>
-            <h1 className="font-bold text-green-600">Create your account</h1>
-            <div className="mt-5 flex flex-col gap-5">
-              <input
-                type="text"
-                id="username"
-                placeholder="username"
-                name="username"
-                className="rounded-sm border border-gray-300 p-2 text-black"
-                required
-              />
-              <input
-                type="email"
-                id="email"
-                placeholder="enter your email address"
-                name="email"
-                className="rounded-sm border border-gray-300 p-2 text-black"
-                required
-              />
 
-              <input
-                type="password"
-                id="password"
-                name="password"
-                placeholder="password"
-                className="rounded-sm border border-gray-300 p-2 text-black"
-                required
-              />
-            </div>
-            <button className="my-[15px] w-full rounded-md bg-blue-400 p-3">
-              Register
-            </button>
-          </form>
-        )}
+            <RxCross1
+              size={25}
+              color="black"
+              onClick={() => navigate("/")}
+              className="cursor-pointer"
+            />
+          </div>
+          <h1 className="font-bold text-green-600">Create your account</h1>
+          <div className="mt-5 flex flex-col gap-5">
+            <input
+              type="text"
+              id="username"
+              placeholder="username"
+              name="username"
+              className="rounded-sm border border-gray-300 p-2 text-black"
+              required
+            />
+            <input
+              type="email"
+              id="email"
+              placeholder="enter your email address"
+              name="email"
+              className="rounded-sm border border-gray-300 p-2 text-black"
+              required
+            />
+
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="password"
+              className="rounded-sm border border-gray-300 p-2 text-black"
+              required
+            />
+          </div>
+          <button className="my-[15px] w-full rounded-md bg-blue-400 p-3">
+            Register
+          </button>
+        </form>
       </div>
       <div>
         <p className="mt-5 text-center">
